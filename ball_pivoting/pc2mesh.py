50bf4d20802d157f4e2bc6fb457331325b89841f
charlierkj <charlierkj@icloud.com>
2019-11-28 18:56:47 -0500

    typo fixed

--- ball_pivoting/pc2mesh.py
+++ ball_pivoting/pc2mesh.py
@@ -3,7 +3,7 @@ import argparse
 import numpy as np
 import pcl
 from mesher import *
-from utils import *
+import utils
 
 
 if __name__ == "__main__":
@@ -15,18 +15,27 @@ if __name__ == "__main__":
                         default=os.path.join(current_dir, 'bunny.obj'))
     parser.add_argument('-of', '--output_file', type=str, \
                         default=os.path.join(current_dir, 'test.obj'))
-    parser.add_argument('-r', '--ball_radius', default=0.04)
+    parser.add_argument('-r', '--ball_radius', nargs='*', default=0.04)
+    args = parser.parse_args()
 
 
     cloud = pcl.PointCloud()
-    print("Reading point cloud")
+    print("Reading point cloud ...")
     if '.txt' in args.input_file:
         points = np.loadtxt(args.input_file)
     elif '.obj' in args.input_file:
         points = utils.read_vertices_from_obj(args.input_file)
+    print("Reading done.")
 
     cloud.from_array(points)
 
-    mesh = Mesher(cloud, args.ball_radius)
-    mesh.reconstruct_with_multi_radius()
+    if isinstance(args.ball_radius, list):
+        radius = [float(r) for r in args.ball_radius]
+        mesh = Mesher(cloud, radius)
+        mesh.reconstruct_with_multi_radius()
+    else:
+        radius = float(args.ball_radius)
+        mesh = Mesher(cloud, radius)
+        mesh.reconstruct()
+        
     utils.save_obj(mesh, args.output_file)
